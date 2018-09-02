8d51998c2f35834674735901feb9d6f0c1d2efe3
Carlos Castillo <castillo.st@gmail.com>
2018-09-02 12:00:41 +0200

    Add content to about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+        <h1>About</h1>
+        <p>
+          This is a simple helper for bookkeeping solar systems, planets and resources for the
+          No Man's Sky video game.
+
+
+        </p>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
 </template>
+
+<style scoped>
+  h1 {
+    font-weight: normal;
+  }
+</style>
